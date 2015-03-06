9fff9aa5ee08328f11d120397501197eba4c69e5
Christian Gibson <cjgibson@users.noreply.github.com>
2015-03-06 00:26:19 -0500

    Replacing tab indents with four-space indents in main.java and LazyPermutationIterator.java

--- 93102618/LazyPermutationIterator/main.java
+++ 93102618/LazyPermutationIterator/main.java
@@ -1,30 +1,30 @@
 import java.util.Arrays;
 
 public class main {
-	public static void main(String[] args) {
-		for (int i = 3 ; i < 6 ; i++) {
-			Integer[] test = range(i);
-			int count = 0;
-			LazyPermutationIterator<Integer> lazy = 
-					new LazyPermutationIterator<Integer>(test);
-			System.out.printf("Permutations of array of size %d (expected %d)",
-					i, lazy.factorial(i));
-			while (lazy.hasNext()) {
-				lazy.next();
-				count += 1;
-				System.out.println();
-				System.out.printf("%d: ", count);
-				System.out.print(lazy);
-			}
-			System.out.println("\n");
-		}
-	}
-	
-	public static Integer[] range(int i) {
-		Integer[] ret = new Integer[i];
-		for (int j = 0 ; j < i ; j++) {
-			ret[j] = j+1;
-		}
-		return ret;
-	}
+    public static void main(String[] args) {
+        for (int i = 3 ; i < 6 ; i++) {
+            Integer[] test = range(i);
+            int count = 0;
+            LazyPermutationIterator<Integer> lazy = 
+                    new LazyPermutationIterator<Integer>(test);
+            System.out.printf("Permutations of array of size %d (expected %d)",
+                    i, lazy.factorial(i));
+            while (lazy.hasNext()) {
+                lazy.next();
+                count += 1;
+                System.out.println();
+                System.out.printf("%d: ", count);
+                System.out.print(lazy);
+            }
+            System.out.println("\n");
+        }
+    }
+    
+    public static Integer[] range(int i) {
+        Integer[] ret = new Integer[i];
+        for (int j = 0 ; j < i ; j++) {
+            ret[j] = j+1;
+        }
+        return ret;
+    }
 }
