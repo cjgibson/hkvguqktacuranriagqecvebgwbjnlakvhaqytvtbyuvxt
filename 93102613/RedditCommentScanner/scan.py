6919b20f2802855db27e401c463d1828dd5b3f14
cjgibson <cjgibson@users.noreply.github.com>
2018-09-20 01:16:29 -0400

    Parallelizing reddit comment pulldown script.

--- 93102613/RedditCommentScanner/scan.py
+++ 93102613/RedditCommentScanner/scan.py
@@ -4,6 +4,7 @@ from os import walk
 from os.path import realpath, join
 from lzma import open as lzma_open
 from time import time
+from multiprocessing import Process, Queue, Pipe, Lock, cpu_count
 
 def iter_files(basepath):
     for dirname, paths, names in walk(realpath(basepath)):
@@ -33,6 +34,7 @@ def scan_bzip_content(file_, substr=None, _chunksize=25 * (1024 ** 2)):
                         yield loads(line.strip())
             chunk = fin.read(_chunksize)
 
+
 def scan_lzma_content(file_, substr=None):
     # xzcat RC_20##-##.xz | grep '"author":"$AUTHOR"' | jq .id -r
     with lzma_open(file_, "rb") as fin:
@@ -40,34 +42,79 @@ def scan_lzma_content(file_, substr=None):
             if substr is None or substr in line:
                 yield loads(line.strip())
 
+
+def scan_content(file_, substr=None):
+    if file_.endswith(".bz2"):
+        return scan_bzip_content(file_, substr)
+    elif file_.endswith(".xz"):
+        return scan_lzma_content(file_, substr)
+    else:
+        raise ValueError("File extension %r is not supported."
+                         % file_.rsplit(".", 1)[-1])
+
+
+def process_target(path_queue, result_pipe, result_lock, author, counting=False):
+    search = b'"author":"%s"' % bytes(author, "ascii")
+    count = 0
+    while True:
+        runtime = time()
+        file_or_none = path_queue.get()
+        if file_or_none is None:
+            break
+        for dictionary in scan_content(file_or_none, search):
+            if dictionary.get("author", None) == author:
+                if counting:
+                    count += 1
+                else:
+                    with result_lock:
+                        result_pipe.send(dictionary["id"])
+        if counting:
+            with result_lock:
+                result_pipe.send((count, file_or_none, time() - runtime))
+            count = 0
+    with result_lock:
+        result_pipe.send(None)
+
+
+"""
+  Example usage:
+    $ python3 scan.py $AUTHOR
+
+    $ python3 scan.py $AUTHOR --extract > comment.identifiers
+    $ wc -l comment.identifiers
+"""
+
+
 if __name__ == "__main__":
     from sys import argv
-    runtime = time()
     author = argv[1]
-    search = b'"author":"%s"' % bytes(author, "ascii")
     counting = False if ("--extract" in argv[1:] or "-e" in argv[1:]) else True
+    path_queue = Queue()
+    result_pull, result_push = Pipe(duplex=False)
+    result_lock = Lock()
+    processes = []
+    process_count = cpu_count()
+
     for name in iter_files("."):
-        localtime = time()
-        if counting:
-            print("Scanning reddit archive at %r." % name, end=" ", flush=True)
-        if name.endswith(".bz2"):
-            content = scan_bzip_content(name, search)
-        elif name.endswith(".xz"):
-            content = scan_lzma_content(name, search)
-        else:
-            if counting:
-                print("   Skipping due to unsupported extension.")
+        path_queue.put(name)
+
+    for _ in range(process_count):
+        processes.append(
+            Process(target=process_target,
+                    args=(path_queue, result_push, result_lock, author, counting)))
+        processes[-1].daemon = True
+        processes[-1].start()
+        path_queue.put(None)
+
+    while process_count > 0:
+        id_or_none = result_pull.recv()
+        if id_or_none is None:
+            process_count -= 1
             continue
         if counting:
-            print(
-                "Found %d matching records (in %.3f seconds)."
-                % (sum(1 for dictionary in content
-                       if dictionary.get("author", None) == author),
-                   time() - localtime)
-            )
+            print("Found %d matching records from %r. (in %.3f seconds)" % id_or_none)
         else:
-            for dictionary in content:
-                if dictionary.get("author", None) == author:
-                    print(dictionary["id"])
-    if counting:
-        print("Total runtime: %.3f seconds." % (time() - runtime))
+            print(id_or_none)
+
+    for process in processes:
+        process.join()
